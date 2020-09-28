730bc5159ccabde0bed84e811a779a02f1ccaf3e
Smari <ellertsmari@gmail.com>
2020-09-28 15:39:37 +0000

    committing after zoom meeting

---
+++ js/events.js
@@ -0,0 +1,17 @@
+console.log(`test
+writing code in more than one line
+also here
+`);
+
+//get the input with id="fname" to work with it in JavaScript
+var firstNameEl = document.getElementById("fname");
+//var firstNameEl = document.querySelector("input#fname");
+console.log(firstNameEl)
+
+//console.log the value written in the input field
+function functionName(e){
+    e.preventDefault();
+    console.log(e)
+    var inputVal = firstNameEl.value;
+    console.log(inputVal);
+}
